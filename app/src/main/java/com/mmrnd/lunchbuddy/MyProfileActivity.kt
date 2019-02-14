b6597d87e868958a1d479bab491cb4a712a42d9e
Esteban Sosa <esteban.sosa96@gmail.com>
2019-02-14 13:45:40 -0500

    Changes

---
+++ app/src/main/java/com/mmrnd/lunchbuddy/MyProfileActivity.kt
@@ -0,0 +1,16 @@
+package com.mmrnd.lunchbuddy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Esteban Sosa
+ */
+
+class MyProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+    }
+}
