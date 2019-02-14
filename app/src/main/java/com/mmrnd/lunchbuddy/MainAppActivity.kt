5d9e60f986e116610ca8806ca04a6e1363169b8e
Esteban Sosa <esteban.sosa96@gmail.com>
2019-02-14 11:41:42 -0500

    Changes

---
+++ app/src/main/java/com/mmrnd/lunchbuddy/MainAppActivity.kt
@@ -0,0 +1,16 @@
+package com.mmrnd.lunchbuddy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Esteban Sosa
+ */
+
+class MainAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_app)
+    }
+}
