1dcd848ff51c80879a9ec08c86f024f407f338f9
Esteban Sosa <esteban.sosa96@gmail.com>
2019-02-15 14:04:42 -0500

    Changes

---
+++ app/src/main/java/com/mmrnd/lunchbuddy/DatabaseManager.kt
@@ -0,0 +1,22 @@
+package com.mmrnd.lunchbuddy
+
+/**
+ * Created by Esteban Sosa
+ */
+
+object DatabaseManager {
+    val INTERESTS = "Interests"
+    val ADDED_BY = "AddedBy"
+    val INTEREST_USERS = "InterestUsers"
+    val USERS = "Users"
+    val NAME = "Name"
+    val EMAIL = "Email"
+    val USER_PHOTO = "UserPhoto"
+    val LOCATION = "Location"
+    val USER_INTERESTS = "UserInterests"
+    val USER_INTEREST_LEVEL = "UserInterestLevel"
+    val USER_INTEREST_DETAILS = "UserInterestDetails"
+    val NOVICE = 0
+    val OK = 1
+    val EXPERT = 2
+}
--- app/src/main/java/com/mmrnd/lunchbuddy/MainAppActivity.kt
+++ app/src/main/java/com/mmrnd/lunchbuddy/MainAppActivity.kt
@@ -2,6 +2,12 @@ package com.mmrnd.lunchbuddy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
+import android.support.v7.widget.Toolbar
+import android.widget.TextView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 /**
  * Created by Esteban Sosa
@@ -9,8 +15,29 @@ import android.os.Bundle
 
 class MainAppActivity : AppCompatActivity() {
 
+    // GUI elements
+    var toolbar: Toolbar? = null
+    var searchView: SearchView? = null
+    var recyclerView: RecyclerView? = null
+    var findNowButton: TextView? = null
+
+    // Variables
+    var interests = ArrayList<String>()
+
+    // Firebase
+    var ref: DatabaseReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app)
+
+        // Find GUI elements
+        toolbar = findViewById(R.id.main_app_toolbar)
+        searchView = findViewById(R.id.main_app_searchview)
+        recyclerView = findViewById(R.id.main_app_recyclerview)
+        findNowButton = findViewById(R.id.main_app_findsomeone_button)
+
+        // Initialize Firebase
+        ref = FirebaseDatabase.getInstance().reference
     }
 }
