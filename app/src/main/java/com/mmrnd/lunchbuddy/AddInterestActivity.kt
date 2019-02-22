ddeeb1e6a857df173da9dbb685a08f5f2248895b
Esteban Sosa <esteban.sosa96@gmail.com>
2019-02-22 13:53:14 -0500

    Changes

--- app/src/main/java/com/mmrnd/lunchbuddy/AddInterestActivity.kt
+++ app/src/main/java/com/mmrnd/lunchbuddy/AddInterestActivity.kt
@@ -2,10 +2,13 @@ package com.mmrnd.lunchbuddy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.SearchView
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.MenuItem
+import com.google.firebase.database.*
 
 /**
  * Created by Esteban Sosa
@@ -17,6 +20,13 @@ class AddInterestActivity : AppCompatActivity() {
     var toolbar: Toolbar? = null
     var searchView: SearchView? = null
     var recyclerView: RecyclerView? = null
+
+    // Firebase
+    var ref: DatabaseReference? = null
+
+    // Variables
+    var interests = ArrayList<String>()
+    var interestsAdapter: InterestsAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +41,48 @@ class AddInterestActivity : AppCompatActivity() {
         toolbar!!.title = ""
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        // Initialize Firebase
+        ref = FirebaseDatabase.getInstance().reference.child(DatabaseManager.INTERESTS)
+
+        // Initialize variables
+        recyclerView!!.layoutManager = LinearLayoutManager(this)
+        interests = ArrayList()
+        interestsAdapter = InterestsAdapter(this, interests, object: InterestsAdapter.OnInterestInterface {
+            override fun interestClicked(index: Int) {
+                TODO("not implemented")
+            }
+        })
+        recyclerView!!.adapter = interestsAdapter
+        interestsAdapter!!.notifyDataSetChanged()
+
+        // Fetch interests
+        fetchInterests()
+    }
+
+    // Fetch interests
+    private fun fetchInterests() {
+        // Clear list and recycler view
+        interests.clear()
+        interestsAdapter!!.notifyDataSetChanged()
+        // Retrieve info
+        ref!!.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()) {
+                    for(childSnap in snapshot.children) {
+                        Log.d("INTEREST", childSnap.key.toString())
+                        interests.add(childSnap.key.toString())
+                        interestsAdapter!!.notifyDataSetChanged()
+                    }
+                }
+                else {
+                    Log.d("TAG", "NOT EXISTS")
+                }
+            }
+            override fun onCancelled(snapshot: DatabaseError) {
+                Log.d("TAG", "ERROR")
+            }
+        })
     }
 
     // Menu methods
